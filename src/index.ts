3377ede66cf6b0a8a63c3e9163a1cb54a8c60740
Michaela Stopková <stopkova.michaela083@gmail.com>
2025-09-29 18:03:39 +0200

    bmiIndex

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,39 @@
+//m = telesna hmotnost v kilogramoch
+//h = telesna vyska v metroch
+function bmiIndex(m: number, h: number){
+    let BMI = m /(h ** 2);
+    //console.log(BMI)
 
-type Person = {firstName: string; secondName: string}
+    let minNormalnaHmotnost: number = 19 * (h ** 2);
+    let maxNormalnahmotnost: number = 25 * (h ** 2);
 
-const p1: Person = {
-    firstName: 'Jan',
-    secondName: 'Mrkvicka'
+    let stav: string;
+    let odporucanie: string = " ";
+
+    if (BMI<=19) {
+        stav = "podváhu"
+        let pribrat: number = +(minNormalnaHmotnost - m).toFixed(2);
+        odporucanie = `Mali by ste pribrať ${pribrat} kg.`
+    } else if (BMI <= 25){
+        stav = "normálnu hmotnosť"
+    } else if (BMI <= 30) {
+        stav = "nadváhu"
+        let schudnut: number = +(maxNormalnahmotnost - m).toFixed(2);
+        odporucanie = `Mali by ste schudnúť ${schudnut} kg.`
+    } else {
+        stav = "obezitu"
+        let schudnut: number = +(maxNormalnahmotnost - m).toFixed(2);
+        odporucanie = `Mali by ste schudnúť ${schudnut} kg.`
+    }
+
+    return `Vaše BMI je ${BMI.toFixed(2)}, čo predstavuje ${stav}. ${odporucanie}`;
 }
 
-console.log(p1)
+console.log(bmiIndex(50, 1.75))
+console.log(bmiIndex(95, 1.90))
+
+
+
+
+
+
