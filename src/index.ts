c4f29baf0470861930aa546d24b460e723116816
Michaela Stopková <stopkova.michaela083@gmail.com>
2025-10-06 22:11:34 +0200

    fibonacci

--- src/index.ts
+++ src/index.ts
@@ -29,19 +29,62 @@ function bmiIndex(m: number, h: number){
     return `Vaše BMI je ${BMI.toFixed(2)}, čo predstavuje ${stav}. ${odporucanie}`;
 }
 
-// console.log(bmiIndex(50, 1.75))
-// console.log(bmiIndex(95, 1.90))
+console.log(bmiIndex(50, 1.75))
+console.log(bmiIndex(95, 1.90))
+
+
 
 /* Na vstupe su cele cisla, funkcia vypocita n-tu mocninu cisla a. */
 function mocnina(a: number, n: number): number{
     return  a ** n;
 }
 
-// console.log(mocnina(-2, 5))
+console.log(mocnina(-2, 5))
 
 
 
+function fibonacci(n: number) : number[]  {
+    let a : number = 0;
+    let b : number = 1;
+    let sum : number;
+    let numbers : number[];
 
+    if (n <= 1){
+        return numbers = [a];
+    } else if (n == 2){
+        return numbers = [a, b]
+    } else {
+        numbers = [a, b];
+        for (let i = 2; i < n; i++){
+            sum = a + b;
+            numbers.push(sum);
+            a = b;
+            b = sum;
+        }
+        return numbers;
+    }
+}
+
+console.log(fibonacci(11));
+
+
+
+function faktorial(n:number) : number | string {
+    let pom : number = 1;
+
+    if (n <= 0){
+        return "Zadaj celé číslo > 0"
+    } else {
+        while (n > 0){
+            pom *= n;
+            n --;
+        }
+        return pom;
+    }
+}
+
+console.log(faktorial(5))
+console.log(faktorial(0))
 
 
 
