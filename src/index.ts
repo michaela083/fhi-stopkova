64758840e8e7ca24561fbf5f073867629224b076
Michaela Stopková <stopkova.michaela083@gmail.com>
2025-12-08 20:33:39 +0100

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import {Request, Response} from 'express';
 import {
     createUser,
@@ -26,6 +27,7 @@ import {
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
